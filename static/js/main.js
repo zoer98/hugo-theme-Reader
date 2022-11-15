6512d13f82731a5a062ab87318abce273f242074
zburu <shuxhan@163.com>
2022-11-15 21:14:59 +0800

    修复灯箱状态下页面滚动的错误

--- static/js/main.js
+++ static/js/main.js
@@ -1,25 +1,25 @@
 $('.post-content img').addClass('slb')
 $('.slb').simplebox({
-  fadeSpeed: 400
+  fadeSpeed: 100
 });
 
 $(window).off('scroll');
-(function () {
-  let progress_bar = $('.progress-bar');
-  let win_h = $(window).height();
-  let body_h = $('body').height();
-  let sHeight = body_h - win_h;
 
-  function updateProgress(p) {
-    progress_bar.css('width', p * 100 + '%');
-  }
-  $(window).on('scroll', function () {
-    window.requestAnimationFrame(function () {
-      let perc = Math.max(0, Math.min(1, $(window).scrollTop() / sHeight));
-      updateProgress(perc);
-    });
+let progress_bar = $('.progress-bar');
+let win_h = $(window).height();
+let body_h = $('body').height();
+let sHeight = body_h - win_h;
+
+function updateProgress(p) {
+  progress_bar.css('width', p * 100 + '%');
+}
+$(window).on('scroll', function () {
+  window.requestAnimationFrame(function () {
+    let perc = Math.max(0, Math.min(1, $(window).scrollTop() / sHeight));
+    updateProgress(perc);
   });
-}());
+});
+
 
 $('#toTop').on('click', function () {
   $("html, body").animate({ scrollTop: 0 }, { duration: 500, easing: "swing" });
