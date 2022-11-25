7a7c5cc20fcb34795eb5151b839477a978f23d57
zburu <shuxhan@163.com>
2022-11-25 20:43:18 +0800

    新增字体风格，大小切换设置

--- static/js/main.js
+++ static/js/main.js
@@ -3,19 +3,65 @@ $('.slb').simplebox({
   fadeSpeed: 100
 });
 
-$(window).off('scroll');
-
-let progress_bar = $('.progress-bar');
-let win_h = $(window).height();
-let body_h = $('body').height();
-let sHeight = body_h - win_h;
-
-function updateProgress(p) {
-  progress_bar.css('width', p * 100 + '%');
-}
-$(window).on('scroll', function () {
-  window.requestAnimationFrame(function () {
-    let perc = Math.max(0, Math.min(1, $(window).scrollTop() / sHeight));
-    updateProgress(perc);
-  });
+$(".totop").click(function () { $("html,body").animate({ scrollTop: 0 }, 500); });
+$(window).on("scroll", function () {
+  if ($(window).scrollTop() >= 200) {
+    $('.totop').addClass('is-active')
+  } else {
+    $('.totop').removeClass('is-active')
+  }
 });
+
+
+$(".utool .item.setting span").click(function () {
+  $(".utool .item.setting .box").toggle();
+});
+
+const ulid = document.getElementById('ulid');
+const FontFamily = ['LXGWWenKaiLite', 'OPPOSans']
+const lis = ulid.getElementsByTagName("li");
+const bodyff = JSON.parse(localStorage.getItem("font-family"));
+
+if (bodyff != null) {
+  $('body').css('font-family', bodyff[1]);
+  $(lis[(bodyff[0])]).addClass('active');
+  if (lis[(bodyff[0])] == false) {
+    $('.utool .item.setting .box ul li:nth-child(1)').addClass('active')
+  }
+}
+
+for (var i = 0, len = lis.length; i < len; i++) {
+  (function (i) {
+    lis[i].onclick = function () {
+      $(lis).removeClass('active')
+      $(lis[i]).addClass('active');
+      $('body').css('font-family', FontFamily[i]);
+
+      // 存入
+      const arr = [i, FontFamily[i]];
+      localStorage.setItem("font-family", JSON.stringify(arr));
+    }
+  })(i);
+}
+
+// 字体大小
+const bodyfz = JSON.parse(localStorage.getItem("font-size"));
+if (bodyfz != null) {
+  $('.markdown-body').css('font-size', bodyfz)
+}
+
+var fontsize = parseInt($('.markdown-body').css('font-size').replace('px', ''))
+$('#ulfz li:nth-child(1)').click(function () {
+  if (fontsize > 12) {
+    fontsize = fontsize - 1;
+    $('.markdown-body').css('font-size', fontsize)
+  }
+  localStorage.setItem("font-size", JSON.stringify(fontsize));
+})
+$('#ulfz li:nth-child(2)').click(function () {
+  if (fontsize < 24) {
+    fontsize = fontsize + 1;
+    $('.markdown-body').css('font-size', fontsize)
+  }
+  localStorage.setItem("font-size", JSON.stringify(fontsize));
+})
